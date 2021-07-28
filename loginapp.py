6b0b0b9adb22e364b259960eb5fff54ae3d36c41
baverkacar <74879967+baverkacar@users.noreply.github.com>
2021-07-28 10:37:47 +0300

    Main files added

---
+++ loginapp.py
@@ -0,0 +1,185 @@
+import random
+import sys
+import sqlite3
+import webbrowser
+
+from PyQt5.uic import loadUi
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QDialog, QApplication, QWidget, QFileDialog
+from PyQt5.QtGui import QPixmap
+
+
+class WelcomeScreen(QDialog):
+    def __init__(self):
+        super(WelcomeScreen, self).__init__()
+        loadUi("Welcomescreen.ui", self)
+        self.login.clicked.connect(lambda: self.openLoginScreen())
+        self.create.clicked.connect(lambda: self.openCreateAccScreen())
+
+    def openLoginScreen(self):
+        login = LoginScreen()
+        widget.addWidget(login)
+        widget.setCurrentIndex(widget.currentIndex() + 1)
+
+    def openCreateAccScreen(self):
+        create = CreateAccScreen()
+        widget.addWidget(create)
+        widget.setCurrentIndex(widget.currentIndex() + 1)
+
+
+class LoginScreen(QDialog):
+    def __init__(self):
+        super(LoginScreen, self).__init__()
+        loadUi("login.ui", self)
+        self.passwordfield.setEchoMode(QtWidgets.QLineEdit.Password)
+        self.login.clicked.connect(lambda: self.loginButtonClicked())
+
+    def loginButtonClicked(self):
+        user = self.emailfield.text()
+        password = self.passwordfield.text()
+
+        if len(user) == 0 or len(password) == 0:
+            self.error.setText("Please input all fields.")
+
+        else:
+            conn = sqlite3.connect("login_app.db")
+            cur = conn.cursor()
+            query = 'SELECT password FROM login_info WHERE username =\'' + user + "\'"
+            email = 'SELECT email FROM login_info WHERE username =\'' + user + "\'"
+            cur.execute(query)
+            result_pass = cur.fetchone()[0]
+            if result_pass == password:
+                cur.execute(email)
+                result_pass = cur.fetchone()[0]
+                self.openEmailCode(result_pass)
+                self.error.setText("")
+            else:
+                self.error.setText("Invalid username or password")
+
+    def openEmailCode(self, email):
+        emailScreen = EmailScreen(email)
+        widget.addWidget(emailScreen)
+        widget.setCurrentIndex(widget.currentIndex() + 1)
+
+
+class CreateAccScreen(QDialog):
+    def __init__(self):
+        super(CreateAccScreen, self).__init__()
+        loadUi("createacc.ui", self)
+        self.passwordfield.setEchoMode(QtWidgets.QLineEdit.Password)
+        self.confirmpasswordfield.setEchoMode(QtWidgets.QLineEdit.Password)
+        self.signup.clicked.connect(lambda: self.signUpButtonClicked())
+
+    def signUpButtonClicked(self):
+        user = self.userfield.text()
+        password = self.passwordfield.text()
+        email = self.emailfield.text()
+        confirmpassword = self.confirmpasswordfield.text()
+
+        if len(user) == 0 or len(password) == 0 or len(confirmpassword) == 0 or len(email) == 0:
+            self.error.setText("Please fill in all inputs.")
+
+        elif password != confirmpassword:
+            self.error.setText("Passwords do not match.")
+        else:
+            conn = sqlite3.connect("login_app.db")
+            cur = conn.cursor()
+
+            cur.execute("Insert into login_info values (?,?,?)", (user, password, email))
+
+            conn.commit()
+            conn.close()
+
+            fillprofile = FillProfileScreen(user, email)
+            widget.addWidget(fillprofile)
+            widget.setCurrentIndex(widget.currentIndex() + 1)
+
+
+class EmailScreen(QDialog):
+    def __init__(self, email):
+        self.email = email
+        super(EmailScreen, self).__init__()
+        loadUi("emailCode.ui", self)
+        import smtplib
+        from email.mime.multipart import MIMEMultipart
+        from email.mime.text import MIMEText
+
+        message = MIMEMultipart()
+        message["From"] = "loginappbaver@gmail.com"
+        message["To"] = email
+        message["Subject"] = "Baver Kaçar Login App Entrance"
+        randomCode = random.randint(1000, 9999)
+        text = "You need to enter the code down below for login to app:\n{}".format(randomCode)
+        message_body = MIMEText(text, "plain")
+        message.attach(message_body)
+        try:
+            mail = smtplib.SMTP("smtp.gmail.com", 587)
+            mail.ehlo()
+            mail.starttls()
+            print(123123)
+            mail.login("loginappbaver", "loginapp11-")
+            print(12)
+            mail.sendmail(message["From"], message["To"], message.as_string())
+            print("mail sent successfully")
+            mail.close()
+
+        except:
+            sys.stderr.write("Problem occurred")
+            sys.stderr.flush()
+        self.login.clicked.connect(lambda: self.emailLoginButton(randomCode))
+
+    def emailLoginButton(self, randomCode):
+        text = self.emailfield.text()
+        if text == str(randomCode):
+            self.error.setText("Logged in Successfully")
+            baverkacar = BaverKacar()
+            widget.addWidget(baverkacar)
+            widget.setCurrentIndex(widget.currentIndex() + 1)
+        else:
+            self.error.setText("Code is wrong")
+
+
+class BaverKacar(QDialog):
+    def __init__(self):
+        super(BaverKacar, self).__init__()
+        loadUi("baverkacar.ui", self)
+        self.github.clicked.connect(lambda: webbrowser.open("https://github.com/baverkacar"))
+        self.linkedin.clicked.connect(
+            lambda: webbrowser.open("https://www.linkedin.com/in/baver-ka%C3%A7ar-b14460187/"))
+
+
+class FillProfileScreen(QDialog):
+    def __init__(self, userText, emailText):
+        super(FillProfileScreen, self).__init__()
+        loadUi("fillprofile.ui", self)
+        self.userText = userText
+        self.emailText = emailText
+        self.username.setText(userText)
+        self.email.setText(emailText)
+        self.image.setPixmap(QPixmap('empty-profile.png'))
+        self.cont.clicked.connect(lambda: self.openLoginScreen())
+        self.upload.clicked.connect(lambda: self.on_click())
+
+    def openLoginScreen(self):
+        login = LoginScreen()
+        widget.addWidget(login)
+        widget.setCurrentIndex(widget.currentIndex() + 1)
+
+    def on_click(self):
+        print('PyQt5 button click')
+        image = QFileDialog.getOpenFileName(None, 'OpenFile', '', "Image file(*.jpg)")
+        imagePath = image[0]
+        pixmap = QPixmap(imagePath)
+        self.label.setPixmap(pixmap)
+        print(imagePath)
+
+
+# main
+app = QApplication(sys.argv)
+welcome = WelcomeScreen()
+widget = QtWidgets.QStackedWidget()
+widget.addWidget(welcome)
+widget.setFixedHeight(800)
+widget.setFixedWidth(1200)
+widget.show()
+sys.exit(app.exec_())
